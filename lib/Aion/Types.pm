ef5701172b2f5b3c830858904d5964716622c287
Yaroslav O. Kosmina <dart@cpan.org>
2025-05-28 15:15:52 +0300

    master test Double with bignum

--- lib/Aion/Types.pm
+++ lib/Aion/Types.pm
@@ -316,8 +316,12 @@ subtype "Any";
 				subtype "NumLike", where { looks_like_number($_) };
 					subtype "Float", as &NumLike, where { -3.402823466E+38 <= $_ && $_ <= 3.402823466E+38 };
 					subtype "Double", as &NumLike, where {
-						#my $x = (!$^V or $^V lt 5.30.0) && !ref $_ ? do { require 'Math::BigFloat'; Math::BigFloat->new($_) }: $_;
-						-1.7976931348623158e+308 <= $_ && $_ <= 1.7976931348623158e+308 };
+						if(!$^V or $^V lt 5.30.0) {
+                            use bignum;
+                            return -1.7976931348623158e+308 <= $_ && $_ <= 1.7976931348623158e+308;
+						}
+						-1.7976931348623158e+308 <= $_ && $_ <= 1.7976931348623158e+308
+                    };
 					subtype "Range[from, to]", as &NumLike, where { A <= $_ && $_ <= B };
 
 					my $_8bits;
